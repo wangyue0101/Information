3efda5917ae4bb8f23c6aca30aba189d228af3bb
xiaomianyang111 <496345281@qq.com>
2018-12-17 16:41:37 +0800

    配置应用程序命令行启动与数据库迁移

--- manage.py
+++ manage.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
-from flask_script import Manager
 from flask_session import Session
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 
 class Config(object):
@@ -33,6 +34,11 @@ app.config.from_object(Config)
 # 生成MYSQL的数据库对象
 db = SQLAlchemy(app)
 
+# 设置应用程序在命令行启动与数据库迁移
+manager = Manager(app)
+Migrate(app, db)
+manager.add_command("db", MigrateCommand)
+
 # 创建Redis的对象
 redis_store = StrictRedis(host=Config.HOST, port=Config.PORT, decode_responses=True)
 
@@ -43,4 +49,4 @@ def Index():
 
 
 if __name__ == "__main__":
-    app.run()
+    manager.run()
